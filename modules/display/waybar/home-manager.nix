11edf4a72ef5ebde1c4b05725f479a6e2c790841
Callum <thecallummckenna@proton.me>
2025-09-14 18:15:01 -0600

    gatsby waybar

--- modules/display/waybar/home-manager.nix
+++ modules/display/waybar/home-manager.nix
@@ -1,5 +1,40 @@
 { ... }:
 
 {
-  programs.waybar.enable = true;
+  programs.waybar = {
+    enable = true;
+    mainBar.settings = {
+      modules-left = [ "clock" "network" "pulseaudio" ];
+      modules-center = [ "sway/workspaces" ];
+      modules-right = [ "temperature" "cpu" "memory" "group/disk" "battery" ];
+
+      # Disk group
+      "disk" = {
+        format = "Disk: {percentage_used}%";
+      };
+
+      "disk#config" = {
+        path = "~/config";
+        format = "Config: {used}";
+      };
+
+      "disk#code" = {
+        path = "~/code";
+        format = "Code: {used}";
+      };
+
+      "group/disk" = {
+        orientation = "inherit";
+        drawer = {
+          transition-duration = 500;
+          transition-left-to-right = false;
+        };
+        modules = [
+          "disk"
+          "disk#config"
+          "disk#code"
+        ];
+      };
+    };
+  };
 }
