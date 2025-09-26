ad8a11f8c53e54802b6135ca0798ab3c4a4596c7
CallumMcKenna <thecallummckenna@proton.me>
2025-09-26 16:21:25 -0600

    changed browsers

--- modules/nixos/browsers/default.nix
+++ modules/nixos/browsers/default.nix
@@ -1,8 +1,5 @@
 { lib, config, pkgs, ... }:
 
-let
-	inherit (lib) mkEnableOption mkIf;
-in
 {
 	imports = [
 		./bookmarks/ai.nix
@@ -13,33 +10,48 @@ in
 		enable = lib.mkEnableOption "Enable defaultBrowsers" // { default = true; };
 		tor.enable = lib.mkEnableOption "Install Tor-Browser" // { default = true; };
 		librewolf.enable = lib.mkEnableOption "Install LibreWolf" // { default = true; };
-		firefox.enable = lib.mkEnableOption "Install FireFox" // { default = true; };
 	};
 
 	config = lib.mkIf config.browsers.enable {
-		environment.systemPackages = (lib.optionals config.browsers.tor.enable [ pkgs.tor-browser ]) ++ (lib.optionals config.browsers.librewolf.enable [ pkgs.librewolf ]);
+		environment.systemPackages = (lib.optionals config.browsers.tor.enable [ pkgs.tor-browser ]);
 	
-		programs.firefox = lib.mkIf config.browsers.firefox.enable {
+		programs.firefox = lib.mkIf config.browsers.librewolf.enable {
 			enable = true;
-			languagePacks = [ "de" "en-US" ];
-			policies = {
-				AppAutoUpdate = true;
-				BackgroundAppUpdate = true;
-				CaptivePortal = true;
-				Cookies.Behavior = "reject-tracker-and-partition-foreign";
-				DisableAccounts = true;
-				DisableDeveloperTools = false;
-				DisableFirefoxStudies = true;
-				DisableTelemetry = true;
-				ExtensionSettings = {
-					"uBlock0@raymondhill.net" = {
-						install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-						installation_mode = "force_installed";
-					};
-				};
-				HttpsOnlyMode = "enabled";
-				SearchEngines.Default = "DuckDuckGo";
-			};
-		};
+			package = pkgs.librewolf;
+      languagePacks = [ "de" "en-US" ];
+		  policies.Bookmarks = [
+        {
+          Title = "Grok";
+          URL = "https://grok.com/";
+          Folder = "AI";
+        }
+        {
+          Title = "OpenAI";
+          URL = "https://openai.com/";
+          Folder = "AI";
+        }
+        {
+          Title = "Claude";
+          URL = "https://claude.ai/";
+          Folder = "AI";
+        }
+
+        {
+          Title = "Apps";
+          URL = "https://apps.ccsdut.net/";
+          Folder = "School";
+        }
+        {
+          Title = "PowerSchool";
+          URL = "https://powerschool.ccsdut.org/";
+          Folder = "School";
+        }
+        {
+          Title = "Canvas";
+          URL = "https://ccsdut.instructure.com/";
+          Folder = "School";
+        }
+      ];
+    };
 	};
 }
