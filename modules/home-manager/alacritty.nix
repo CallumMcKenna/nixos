0987eed55b6770acbf42bb1dc54cfeda60ea699c
Callum <thecallummckenna@proton.me>
2025-10-12 17:03:02 -0600

    jetbrains mono now default font

--- modules/home-manager/alacritty.nix
+++ modules/home-manager/alacritty.nix
@@ -4,10 +4,17 @@
   options.defaultPkgs.alacritty.enable = lib.mkEnableOption "Enable the default home manager pkgs" // { default = true; };
 
   config = lib.mkIf config.defaultPkgs.enable { # Declared in defaultPkgs.nix
+    fonts.fontconfig.enable = true;
     programs.alacritty = lib.mkIf config.defaultPkgs.alacritty.enable {
       enable = true;
       theme = "gruvbox_material";
-      settings.window.opacity = 0.9;
+      settings = {
+        window.opacity = 0.9;
+        font.normal = {
+          family = "Jetbrains Mono Nerd Font";
+          style = "Regular";
+        };
+      };
     };
   };
 }
