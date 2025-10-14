44b08c96f71e70c0f8617d26cbaa04566d6fcb04
CallumMcKenna <thecallummckenna@proton.me>
2025-10-13 22:04:57 -0600

    tlou plasma theme

--- modules/home-manager/alacritty.nix
+++ modules/home-manager/alacritty.nix
@@ -1,13 +1,16 @@
 { lib, config, ... }:
 
 {
-  options.defaultPkgs.alacritty.enable = lib.mkEnableOption "Enable the default home manager pkgs" // { default = true; };
+  options = {
+    defaultPkgs.alacritty.enable = lib.mkEnableOption "Enable the default home manager pkgs" // { default = true; };
+    alacritty.colorscheme.nord.enable = lib.mkEnableOption "Changes from gruvbox to nord(gruvbox is the default)" // { default = false; };
+  };
 
   config = lib.mkIf config.defaultPkgs.enable { # Declared in defaultPkgs.nix
     fonts.fontconfig.enable = true;
     programs.alacritty = lib.mkIf config.defaultPkgs.alacritty.enable {
       enable = true;
-      theme = "gruvbox_material";
+      theme = if config.alacritty.colorscheme.nord.enable then "nord" else "gruvbox_material";
       settings = {
         window.opacity = 0.9;
         font.normal = {
